dc39d535761fc5ed040eded3556a01d6de553f2e
CatPython1234 <grisanikon@gmail.com>
2024-03-12 23:18:12 +0300

    Add fiel_stepic

---
+++ Циклы(for, while).py
@@ -0,0 +1,45 @@
+# Циклы служат для повторения кода
+# for, while
+
+# FOR:
+
+#for number in 1, 2, 3, 4, 5:
+    #print(number)
+
+#for i in range(5):
+    #print(i)
+    #i += 1
+
+
+#WHILE:
+
+#i = 1
+
+#while i < 5:
+    #print(i)
+    #i += 1
+
+
+# Циклы можно вкладывать в друг друга:
+
+#for i in range(5):
+    #for a in range(10):
+        #print(a)
+    #print(i)
+
+
+#for i in range(11):
+    #if i % 2 == 0:
+        #print('четное')
+    #else:
+        #continue
+
+
+
+#for i in range(11):
+    #if i == 9:
+        #print('девятка')
+        #break
+    #else:
+        #print(i)
+        #continue
